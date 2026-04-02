dd2ab8076e615672e879b491d45f72950db3ff23
Neocr8v <neocr8vmedia@gmail.com>
2026-04-02 19:51:01 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    // Allow unoptimized images if you are using many external local-relative paths still
+    unoptimized: process.env.NODE_ENV === 'development'
+  },
 };
 
 export default nextConfig;
